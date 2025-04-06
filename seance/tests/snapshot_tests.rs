5c4a9d6ea24d2ba0ec86ae357376934fbf6d91ab
Laura Demkowicz-Duffy <dev@demkowiczduffy.co.uk>
2025-04-06 20:34:10 +0100

    feat(seance): snapshot testing for all top-level stages

--- seance/tests/snapshot_tests.rs
+++ seance/tests/snapshot_tests.rs
@@ -4,6 +4,8 @@ fn hackspace_logo() {
         &std::fs::read("../logo.svg").expect("failed to read logo SVG file"),
     )
     .expect("failed to parse logo SVG data");
+
+    let design_name = "York Hackspace Logo";
     let offset = seance::DesignOffset { x: 0.0, y: 0.0 };
 
     let mut tool_passes = seance::default_passes::default_passes();
@@ -12,10 +14,20 @@ fn hackspace_logo() {
     }
 
     let paths = seance::svg::get_paths_grouped_by_colour(&design_file);
-    let mut paths_in_mm = seance::resolve_paths(&paths, &offset, 1.0);
-    seance::paths::filter_paths_to_tool_passes(&mut paths_in_mm, &tool_passes);
-    let resolved_paths = seance::paths::convert_points_to_plotter_units(&paths_in_mm);
-    let hpgl = seance::hpgl::generate_hpgl(&resolved_paths, &tool_passes);
+    insta::assert_debug_snapshot!("york hackspace logo SVG paths", &paths);
 
-    insta::assert_snapshot!("york hackspace logo SVG HPGL output", hpgl);
+    let mut paths_in_mm = seance::resolve_paths(&paths, &offset, 1.0);
+    insta::assert_debug_snapshot!("york hackspace logo resolved paths", &paths_in_mm);
+
+    seance::paths::filter_paths_to_tool_passes(&mut paths_in_mm, &tool_passes);
+    insta::assert_debug_snapshot!("york hackspace logo filtered paths", &paths_in_mm);
+
+    let resolved_paths = seance::paths::convert_points_to_plotter_units(&paths_in_mm);
+    insta::assert_debug_snapshot!("york hackspace logo plotted paths", &resolved_paths);
+
+    let hpgl = seance::hpgl::generate_hpgl(&resolved_paths, &tool_passes);
+    insta::assert_snapshot!("york hackspace logo HPGL output", &hpgl);
+
+    let pcl = seance::pcl::wrap_hpgl_in_pcl(hpgl, design_name, &tool_passes);
+    insta::assert_snapshot!("york hackspace logo PCL output", &pcl);
 }
